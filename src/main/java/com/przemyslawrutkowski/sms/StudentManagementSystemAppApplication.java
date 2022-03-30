bdb64aee911c10f9c22d01eec798d19a2e34516a
szoggun <przrut4@gmail.com>
2022-03-30 20:06:16 +0200

    Initial commit

---
+++ src/main/java/com/przemyslawrutkowski/sms/StudentManagementSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.przemyslawrutkowski.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemAppApplication.class, args);
+    }
+
+}
