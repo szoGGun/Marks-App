fa63cdf1f0e04b8ed29611497b6c67c28a1b60f6
szoggun <przrut4@gmail.com>
2022-04-02 23:10:57 +0200

    Update student feature implemented

--- src/main/java/com/przemyslawrutkowski/sms/controller/StudentController.java
+++ src/main/java/com/przemyslawrutkowski/sms/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -20,12 +21,12 @@ public class StudentController {
 
     @GetMapping("/students")
     public String listStudents(Model model) {
-            model.addAttribute("students", studentService.getAllStudents());
-            return "students";
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
     }
 
     @GetMapping("/students/new")
-    public String createStudentForm(Model model){
+    public String createStudentForm(Model model) {
 
         Student student = new Student();
         model.addAttribute("student", student);
@@ -35,6 +36,27 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id,
+    @ModelAttribute("student") Student student,
+                                Model model) {
+
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        studentService.saveStudent(existingStudent);
         return "redirect:/students";
     }
 }
